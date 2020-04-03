1a47fed7e6f8ac0207277b56ef4052d2320e8d15
Huit/Onze <37984434+HuitOnze@users.noreply.github.com>
2020-04-03 16:25:58 +0200

    Create gutenberg-disable.php

---
+++ gutenberg-disable.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Diasble Gutenberg
+Plugin URI:  https://github.com/HuitOnze/disable-gutenberg
+Description: Small script to deactivate the Gutenberg editor for WordPress.
+Version:     1.0
+Author:      Jérémy Debray
+Author URI:  https://www.huitonze.ch
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+/**
+ * Désactivation de Gutenberg
+ * 
+ * More information:
+ * @link https://digwp.com/2018/04/how-to-disable-gutenberg/
+ */
+// Disable Gutenberg for Post
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+// Disable Gutenberg for Post type
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
